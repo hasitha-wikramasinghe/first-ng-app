debfcb379371438a19affd1eb10fd90dbc224548
hasitha <hasitha.wikramasinghe@gmail.com>
2024-11-15 12:01:42 +0530

    done n dusted

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  title = 'This is from Signal';
+  title = 'My first Angular app';
 }
